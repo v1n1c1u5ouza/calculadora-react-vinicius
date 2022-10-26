2babbaffa031a137b03508f1adaf31587448c86b
v1n1c1us5ouza <viniciusdesouzamarinho.com>
2022-10-26 10:18:25 -0300

    Add: Calculator template to site and button templates

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>Calculadora</div>
+    <Calculator  />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
